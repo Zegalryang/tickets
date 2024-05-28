89eab45b36cb9a18e29661bdcae8d2dee4e9c3d1
이지훈 <fantasy.h@hancom.com>
2024-05-28 17:15:31 +0900

    인터파크 티켓팅 기능 구현
     * 완료
       - 예약하기 후 예약석 지정 화면 열기
    
     * 작업중
       - 예약석 지정하는 화면

--- interpark.py
+++ interpark.py
@@ -1,31 +1,31 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service as ChromeService
-#from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.options import Options
 import time
 import os
 
-# ChromeDriver를 자동으로 다운로드 및 설정
 chrome_options = Options()
-#chrome_options.add_argument("--ignore-certificate-errors")  # SSL 인증서 오류 무시
-#chrome_options.add_argument("--disable-web-security")       # 웹 보안 비활성화
 chrome_options.add_argument("--window-size=1920,1080")
-#driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=chrome_options)
 driver = webdriver.Chrome(service=ChromeService(executable_path='./chromedriver'), options=chrome_options)
 #driver.implicitly_wait(time_to_wait=60)
 
+def login(userId, userPwd):
+    try:
+        driver.get("https://tickets.interpark.com")
+        driver.find_element(By.LINK_TEXT, "로그인").click()
+        e = driver.find_element(By.ID, "userId")
+        e.send_keys(userId)
+        e = driver.find_element(By.ID, "userPwd")
+        e.send_keys(userPwd)
+        driver.find_element(By.ID, "btn_login").click()
+    except Exception as e:
+        return False
 
-try:
-    driver.get("https://tickets.interpark.com")
-    driver.find_element(By.LINK_TEXT, "로그인").click()
-    e = driver.find_element(By.ID, "userId")
-    e.send_keys(os.environ['TICKET_USERID'])
-    e = driver.find_element(By.ID, "userPwd")
-    e.send_keys(os.environ['TICKET_USERPWD'])
-    driver.find_element(By.ID, "btn_login").click()
+    return True
 
-    driver.get("https://tickets.interpark.com/goods/24007162")
+def showBooking(product):
+    driver.get("https://tickets.interpark.com/goods/{}".format(product))
 
     print(f" * Page title: {driver.title}")
 
@@ -61,11 +61,68 @@ try:
     for i, item in enumerate(e):
         print(' * {} 회차 {}: {}'.format(i+1, item.text, item.get_attribute('data-seq')))
 
+    print(' * before window_handles: ', driver.window_handles)
     driver.find_element(By.LINK_TEXT, "예매하기").click()
 
+    while True:
+        if len(driver.window_handles) >= 2: break
+        print(' * 예약 창 기다리는중...-')
+        time.sleep(0.3)
+
+    print(' * 포도알 화면 진입')
+    driver.switch_to.window(driver.window_handles[1])
+    print(' * switch driver title: {}'.format(driver.title))
+
+try:
+    result = login(os.environ['TICKET_USERID'], os.environ['TICKET_USERPWD'])
+    if not result:
+        print(' * Login failure')
+        exit(1)
+
+    showBooking("24007162")
+
+    while True:
+        waiting = len(driver.find_elements(By.CLASS_NAME, "ticketWaiting"))
+        if waiting == 0: break
+
+        print(' * 사용자 큐 대기 상태')
+        time.sleep(0.3)
+
+    frame = driver.find_element(By.ID, 'ifrmSeat')
+    driver.switch_to.frame(frame)
+
+    while True:
+        isExists = len(driver.find_elements(By.ID, 'divRecaptcha'))
+        if not isExists: break
+
+        # When hide set this 'display: none;'
+        if driver.find_element(By.ID, 'divRecaptcha').get_attribute('style') != '': break #
+
+        print(' * Capcha 입력 대기...')
+        time.sleep(0.3)
+
+    print(' * 포도알 선택')
+    frame = driver.find_element(By.ID, 'ifrmSeatDetail')
+    driver.switch_to.frame(frame)
+
+    sections = driver.find_elements(By.CLASS_NAME, 'kcl-user-action')
+    xAxis = [0, 0]
+    yAxis = [0, 0]
+
+    for s in sections:
+        sName = s.get_attribute('onmouseover').split("'")[1]
+        coords = s.get_attribute('coords').split(',')
+
+        for c in coords:
+            xAxis[0] = min(xAxis[0], c[0])
+            xAxis[1] = max(xAxis[1], c[0])
+            yAxis[0] = min(yAxis[0], c[1])
+            yAxis[1] = max(yAxis[1], c[1])
+
     # 몇 초간 대기 (테스트 목적으로)
-    time.sleep(5)
+    # while True: pass
 
 finally:
+    pass
     # 브라우저 닫기
-    driver.quit()
+    # driver.quit()
